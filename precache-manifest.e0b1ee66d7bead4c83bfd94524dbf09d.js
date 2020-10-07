b12c381ad2ba4146055efd665606dba2a10716d9
davevanfleet <vanfleet@arsome.com>
2020-10-07 10:37:37 -0400

    Updates

---
+++ precache-manifest.e0b1ee66d7bead4c83bfd94524dbf09d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4ecd28fad1522056f8dae853e18436",
+    "url": "/field-notebook-sample/index.html"
+  },
+  {
+    "revision": "c64bb1885e491d2f0545",
+    "url": "/field-notebook-sample/static/css/main.aaa96019.chunk.css"
+  },
+  {
+    "revision": "18e1e4891c35e04170b1",
+    "url": "/field-notebook-sample/static/js/2.0f0244e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/field-notebook-sample/static/js/2.0f0244e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c64bb1885e491d2f0545",
+    "url": "/field-notebook-sample/static/js/main.d2795a51.chunk.js"
+  },
+  {
+    "revision": "89ab77d739eedaf7e4e4",
+    "url": "/field-notebook-sample/static/js/runtime-main.001210bd.js"
+  },
+  {
+    "revision": "e9b55ad461e09ff512f13b30799f37ec",
+    "url": "/field-notebook-sample/static/media/bubbleWindow.e9b55ad4.png"
+  },
+  {
+    "revision": "3dbb2029511fb38c8f216edb693d0012",
+    "url": "/field-notebook-sample/static/media/fieldNotesLeft.3dbb2029.png"
+  },
+  {
+    "revision": "004808baa497d6c375652675026379fe",
+    "url": "/field-notebook-sample/static/media/fieldNotesRight.004808ba.png"
+  },
+  {
+    "revision": "adf99c7d12434841ba731fe81cb20a05",
+    "url": "/field-notebook-sample/static/media/goodUnicorn.adf99c7d.ttf"
+  },
+  {
+    "revision": "0945bc04412fbbdae05a068e2848125b",
+    "url": "/field-notebook-sample/static/media/underSea.0945bc04.png"
+  },
+  {
+    "revision": "b048d5f5c107a001a430fab26deb1525",
+    "url": "/field-notebook-sample/static/media/whaleITried.b048d5f5.ttf"
+  }
+]);
